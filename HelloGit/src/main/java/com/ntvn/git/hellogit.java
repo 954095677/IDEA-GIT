b49735d9f2a3fb4372f2e615ecdd484126bb2956
xn <954095677@qq.com>
2020-12-31 12:10:12 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/ntvn/git/hellogit.java
@@ -0,0 +1,11 @@
+package com.ntvn.git;
+
+/**
+ * @author xxxx
+ * @create 2020-12-31-12:03
+ */
+public class hellogit {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
